1526ef22b65c5aac6fb93fae9ee653822e9e680a
MamanGvomi <mamangvomi@gmail.com>
2022-03-25 11:53:42 +0100

    produit scalaire normal

--- SSE/TP1/copy-normal.c
+++ SSE/TP1/copy-normal.c
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+void print_tab(float * tab, int size){
+	printf("Mon tableau : \n");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("[0] : %f\n", tab[i]);
+	}
+	
+}
+
 int copy(int random_size)
 {
 	// Static arrays are stored into the stack thus we need to add an alignment attribute to tell the compiler to correctly align both arrays.
@@ -54,28 +64,51 @@ float reduce_from_tab(int random_size)
 {
 	// init du tableau
 	srand(time(NULL));
-	float * tab = malloc(random_size * sizeof(float));
+	float *tab = malloc(random_size * sizeof(float));
 	for (size_t i = 0; i < random_size; i++)
 	{
 		float value = (float)(rand() % 50);
 		tab[i] = value;
 	}
 
-	//Reduce
+	// Reduce
 	float value = 0.0f;
 	for (size_t i = 0; i < random_size; i++)
 	{
 		value += tab[i];
 	}
 	return value;
+}
 
+float produit_scalaire(int random_size)
+{
+	// init du tableau
+	srand(time(NULL));
+	float *tab = malloc(random_size * sizeof(float));
+	float *tab2 = malloc(random_size * sizeof(float));
+	for (size_t i = 0; i < random_size; i++)
+	{
+		tab[i] = (float)(rand() % 50);
+		tab2[i] = (float)(rand() % 50);
+	}
+
+	float res = 0.0f;
+
+	for (size_t i = 0; i < random_size; i++)
+	{
+		res += tab[i] * tab2[i];
+	}
+	print_tab(tab, random_size);
+	print_tab(tab2, random_size);
+	return res;
 }
 
 int main(void)
 {
 	//   copy(1000000000);
-	printf("\n%f\n", reduce_from_tab(2));
+	// printf("\n%f\n", reduce_from_tab(2));
 	// printf("\n%f\n", maxvalue(10));
+	printf("\n%f\n", produit_scalaire(2));
 
 	return 0;
 }
